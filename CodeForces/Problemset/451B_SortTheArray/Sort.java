d7d120ce46640b400f5b8aef6f41330c7371fd6f
BrandonLMorris <bmorris95@me.com>
2015-07-30 13:35:23 -0500

    Successfully completed CodeForces 451B Sort the Array

--- CodeForces/Problemset/451B_SortTheArray/Sort.java
+++ CodeForces/Problemset/451B_SortTheArray/Sort.java
@@ -1,71 +1,90 @@
 import java.util.*;
 
+/*
+ * From an array of distinct integers, find where, if at all, the array can
+ * be sorted by reversing exactly one segment of the array.
+ */
+
 public class Sort {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
 
-    boolean foundDecreasing = false;
-    boolean decreasingFinished = false;
-    boolean isYes = false;
+    int[] a = new int[n];
+    int[] b = new int[n];
 
-    int[] arr = new int[n];
     for (int i = 0; i < n; i++) {
-      arr[i] = in.nextInt();
+      a[i] = in.nextInt();
+      b[i] = a[i];
     }
 
-    boolean isAllDecreasing = true;
-    for (int i = 0; i < n-1; i++) {
-      if (arr[i] < arr[i+1]) {
-        isAllDecreasing = false;
+    // Sort the array
+    Arrays.sort(b);
+
+    // Map the unsorted elements to their indices
+    HashMap<Integer, Integer> hs = new HashMap<Integer, Integer>();
+    for (int i = 0; i < n; i++) {
+      hs.put(b[i], i);
+    }
+
+    // Set the elements in a to the index of that element in the sorted array 
+    for (int i = 0; i < n; i++) {
+      a[i] = hs.get(a[i]);
+    }
+
+    // Find the inversion from the left
+    int leftInversion = -1;
+    for (int i = 0; i < n; i++) {
+      if (a[i] != i) {
+        leftInversion = i;
         break;
       }
     }
-    isYes = isAllDecreasing;
 
-    int startIndex = 0;
-    int endIndex = 0;
-    for (int i = 0; i < n-1; i++) {
-      if (arr[i] > arr[i+1]) {
-        if (foundDecreasing && !decreasingFinished) {
-          continue;
-        } else if (foundDecreasing && decreasingFinished) {
-          isYes = false;
-          break;
-        } else {
-          foundDecreasing = true;
-          startIndex = i;
-        }
-      } else if (arr[i] < arr[i+1] && foundDecreasing && !decreasingFinished) {
-        decreasingFinished = true;
-        isYes = true;
-        endIndex = i;
+    // Find the inversion from the right
+    int rightInversion = -1;
+    for (int i = n-1; i >= 0; i--) {
+      if (a[i] != i) {
+        rightInversion = i;
+        break;
       }
     }
 
-    System.out.println(isYes ? "yes" : "no"); 
-    if (isAllDecreasing) {
-      startIndex = 1;
-      endIndex = n;
+    // If no inversions, stop here with the answer
+    if (leftInversion == rightInversion) {
+      System.out.println("yes");
+      System.out.println("1 1");
+      return;
+    }
+
+    // Reverse the elements around the inversion elements, and see if that
+    // sorts the array
+    reverseArr(a, leftInversion, rightInversion);
+    boolean sorted = true;
+    for (int i = 0; i < n; i++) {
+      if (i != a[i]) {
+        sorted = false;
+        break;
+      }
+    }
+
+    // Output the result
+    if (sorted){
+      System.out.println("yes");
+      System.out.println((leftInversion + 1) + " " + (rightInversion + 1));
     } else {
-      startIndex++;
-      endIndex++;
+      System.out.println("no");
     }
-    if (isYes) {
-      System.out.println(startIndex + " " + endIndex);
-    }
+
 
   }
+
+  // A helper method to reverse the elements in an array at given indices
+  public static void reverseArr(int[] array, int start, int end) {
+    for (int i = 0; i < (end - start) / 2 + 1; i++) {
+      int temp = array[start + i];
+      array[start + i] = array[end - i];
+      array[end - i] = temp;
+    }
+  }
 }
-
-
-/*
- * [X] Increase only
- * Increase, decrease
- * Increase, decrease, increase
- * Decrease, increase
- * Decrease
- *
- * must be *exactly* one point at which the array decreases
- * mark the beginning an dthe end of the decreasing section
- */
