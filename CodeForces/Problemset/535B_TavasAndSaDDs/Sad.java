aa3a653ac6cf429860b56463bb93b2e940b2438a
BrandonLMorris <bmorris95@me.com>
2015-08-05 10:55:44 -0500

    Successfully completed CodeForces 535B

--- CodeForces/Problemset/535B_TavasAndSaDDs/Sad.java
+++ CodeForces/Problemset/535B_TavasAndSaDDs/Sad.java
@@ -9,34 +9,29 @@ public class Sad {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
-    int n = in.nextInt();
+    // Read the input
+    long n = in.nextLong();
 
-    ArrayList<String> luckies = new ArrayList<String>(510);
-    luckies.add("4");
-    luckies.add("7");
+    // Set up the list of all lucky number <= 10^9
+    ArrayList<Long> luckies = new ArrayList<Long>(1022);
 
-    for (int i = 0; i < 508; i++) {
-      int temp = Integer.parseInt(luckies.get(i));
+    // Add the two first lucky numbers
+    luckies.add(4l);
+    luckies.add(7l);
+
+    // Every lucky number can be constructed by adding either a 4 or a 7
+    // to a previous lucky number
+    for (int i = 0; i < 1020; i++) {
+      String temp = Long.toString(luckies.get(i));
+      luckies.add(Long.parseLong(temp + "4"));
+      luckies.add(Long.parseLong(temp + "7"));
     }
 
+    // Sort the list to be in increasing order
     Collections.sort(luckies);
-    System.out.println((luckies.indexOf(Integer.toString(n)) + 1));
 
-
+    // Print out the one-indexed location of the input
+    System.out.println((luckies.indexOf(n) + 1));
 
   }
 }
-
-/*
- * 4 7 44 47 74 77 444 447 474 477 744 747
- * 0 1 00 01 10 11 000 001 010 011 100
- *
- * 2 1 digit
- * 4 2 digits
- * 8 3 digits
- * 16 4 digits
- * 32 5 digits
- * 64 6 digits
- * 128 7 digits
- * 256 8 digits
- */
