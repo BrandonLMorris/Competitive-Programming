0b8aefd1ac8a8a5eb536935cd6ed729d2b233af0
BrandonLMorris <bmorris95@me.com>
2015-08-06 08:51:25 -0500

    Successfully completed CodeForces 230B

--- CodeForces/Problemset/230B_TPrimes/TPrime.java
+++ CodeForces/Problemset/230B_TPrimes/TPrime.java
@@ -8,17 +8,19 @@ public class TPrime {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
+    // Use the Sieve of Eratosthenes to build up primes under 1000000
     boolean[] isNotPrime = new boolean[1000005];
     isNotPrime[2] = false;
     for (int i = 2; i < 1000005; i++) {
       if (!isNotPrime[i]) {
         int x = i + i;
-        while (x <= 1000005) {
+        while (x < 1000005) {
           isNotPrime[x] = true;
-          x += x;
+          x += i;
         }
       }
     }
+
 
     // Read in the number of numbers
     int n = in.nextInt();
@@ -33,8 +35,8 @@ public class TPrime {
         continue;
       }
 
-      // x has 3 divisors if and only if it is a perfect square. The divisors
-      // will be 1, x, and the square root of x.
+      // x has 3 divisors if and only if it is a perfect square of a prime
+      // number
       double y = Math.sqrt(x);
       if (y == Math.round(y) && !isNotPrime[(int)y]) {
         System.out.println("YES");
