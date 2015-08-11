3f7ca3c6e9f70b3ec1ab85698e80f941abdb5e9e
BrandonLMorris <bmorris95@me.com>
2015-08-11 15:50:15 -0500

    Successfully completed CodeForces 456B

---
+++ CodeForces/Problemset/456B_FedyaAndMaths/maths.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# CodeForces Problem 456B
+# Using Python since it's expanding inteer type helpful for this problem
+
+# Given a number n, find (1^n + 2^n + 3^n + 4^n) mod 5
+
+# Efficient modular exponentiation using right-to-left binary method
+# as described in https:/en.wikipedia.org/wiki/Modular_exponentiation
+def exp_mod(base, exponent, modulus):
+    result = 1
+    base = base % modulus
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = (result * base) % modulus
+        exponent = exponent >> 1
+        base = pow(base, 2) % modulus
+    return result
+
+if __name__ == "__main__":
+    exponent = int(input())
+
+    exponent = exponent % 4
+
+    result = pow(1, exponent) + pow(2, exponent) + pow(3, exponent) + pow(4, exponent)
+    result = result % 5
+
+    print(result)
