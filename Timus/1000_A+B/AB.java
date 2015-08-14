846111bf3f827f02a9da52789ccb4d9f5ec9f1f3
BrandonLMorris <bmorris95@me.com>
2015-08-14 16:22:22 -0500

    Created Timus (acm.timus.ru) folder and successfully completed 1000

---
+++ Timus/1000_A+B/AB.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/*
+ * Given two integers, a and b, find the sum a+b.
+ */
+
+public class AB {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    // Read in the input
+    int a = in.nextInt();
+    int b = in.nextInt();
+
+    // Output the result
+    System.out.println((a+b));
+  }
+}
